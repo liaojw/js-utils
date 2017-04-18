e8c400db4c0d9674627b64ccc1b8f33666d6f703
liaojunwen <297382151@qq.com>
2017-04-18 14:29:14 +0800

    修改Utils.js

--- utils.js
+++ utils.js
@@ -12,7 +12,8 @@
         getUrlParameters: getUrlParameters,
         timestampFormat: timestampFormat,
         getChineseMoney: getChineseMoney,
-        imgBase64Decode: imgBase64Decode
+        imgBase64Decode: imgBase64Decode,
+        getBrowser: getBrowser
     };
     window.$Utils = utils;
 
@@ -225,6 +226,7 @@
             U: function(){return Math.round(jsdate.getTime()/1000)}
         };
         return format.replace(/[\\]?([a-zA-Z])/g, function(t, s){
+            var ret;
             if( t!=s ){
                 ret = s;
             } else if( f[s] ){
@@ -264,6 +266,34 @@
      */
     function imgBase64Decode(data) {
     	return 'data:image/png;base64,' + data;
+    }
+
+    /**
+     * 获取浏览器信息
+     * @returns {{versions: {trident, presto, webKit, gecko, mobile, ios, android, iPhone, iPad, webApp, weixin, alipay, qq}, language: string}}
+     */
+    function getBrowser() {
+        return {
+            versions: function () {
+                var u = navigator.userAgent, app = navigator.appVersion;
+                return {
+                    trident: u.indexOf('Trident') > -1, //IE内核
+                    presto: u.indexOf('Presto') > -1, //opera内核
+                    webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
+                    mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+                    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                    android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+                    iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+                    iPad: u.indexOf('iPad') > -1, //是否iPad
+                    webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+                    weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+                    alipay: u.indexOf('AlipayClient') > -1, //是否为支付宝
+                    qq: u.match(/\sQQ/i) == " qq" //是否QQ
+                };
+            }(),
+            language: (navigator.browserLanguage || navigator.language).toLowerCase()
+        }
     }
 })();
 
