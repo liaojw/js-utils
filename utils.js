0a28a140835c55b149ac15b2e1305b41a740a168
LJW <297382151@qq.com>
2017-05-02 10:44:46 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -274,26 +274,32 @@
      */
     function getBrowser() {
         return {
-            versions: function () {
-                var u = navigator.userAgent, app = navigator.appVersion;
-                return {
-                    trident: u.indexOf('Trident') > -1, //IE内核
-                    presto: u.indexOf('Presto') > -1, //opera内核
-                    webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-                    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
-                    mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-                    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-                    android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
-                    iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
-                    iPad: u.indexOf('iPad') > -1, //是否iPad
-                    webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
-                    weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
-                    alipay: u.indexOf('AlipayClient') > -1, //是否为支付宝
-                    qq: u.match(/\sQQ/i) == " qq" //是否QQ
-                };
-            }(),
-            language: (navigator.browserLanguage || navigator.language).toLowerCase()
-        }
+      navigator: navigator,
+      appVersion: navigator.appVersion,
+      userAgent: navigator.userAgent,
+      language: (navigator.browserLanguage || navigator.language).toLowerCase(),
+      versions: function () {
+        var u = navigator.userAgent;
+        return {
+          trident: u.indexOf('Trident') > -1, //IE内核
+          presto: u.indexOf('Presto') > -1, //opera内核
+          webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+          gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
+          mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+          ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+          android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+          iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+          iPad: u.indexOf('iPad') > -1, //是否为iPad
+          webApp: u.indexOf('Safari') == -1, //是否为web应用程序，没有头部与底部
+          weChat: u.indexOf('MicroMessenger') > -1, //是否为微信
+          alipay: u.indexOf('AlipayClient') > -1, //是否为支付宝
+          weibo: u.indexOf('Weibo') > -1, //是否为微博
+          dingTalk: u.indexOf('DingTalk') > -1, //是否为钉钉
+          qq: u.match(/\sQQ/i) == " qq" //是否为QQ
+
+        };
+      }()
+    }
     }
 })();
 
