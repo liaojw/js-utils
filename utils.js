f1d13ae81e88644d1a6561e6ad7f666be26f79bf
ljw <297382151@qq.com>
2017-09-25 10:34:25 +0800

    修改人名币转换方法函数

--- utils.js
+++ utils.js
@@ -53,6 +53,8 @@
  *      $Utils.getColor(count); 获取随机的颜色数组
  *      $Utils.sortArr(arr) 数组冒泡排序
  *      $Utils.getChineseMoney(val) 获取大写人命币
+ *      $Utils.formatCurrency(val) 将数值四舍五入(保留2位小数)后格式化成金额形式
+ *      $Utils.formatCurrencyTenThou(val) 将数值四舍五入(保留2位小数)后格式化成金额形式
  *      $Utils.getGlobal() 获取宿主环境顶层对象
  */
 // 命名空间
@@ -727,19 +729,69 @@ $Utils.sortArr = function (arr) {
  * @returns {string}
  */
 $Utils.getChineseMoney = function (val) {
-    var dists = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
-    var units = [['元', '万', '亿'], ['', '十', '百', '千']];
+    var fraction = ['角', '分'];
+    var digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
+    var unit = [['元', '万', '亿'], ['', '拾', '佰', '仟']];
+    var head = val < 0 ? '欠' : '';
+    val = Math.abs(val);
     var s = '';
-    for (var i = 0; val > 0 && i < units[0].length; i++) {
+    for (var i = 0; i < fraction.length; i++) {
+        s += (digit[Math.floor(val * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
+    }
+    s = s || '整';
+    val = Math.floor(val);
+    for (var i = 0; i < unit[0].length && val > 0; i++) {
         var p = '';
-        for (var j = 0; val > 0 && j < units[1].length; j++) {
-            var left = val % 10;
-            p = dists[left] + units[1][j] + p;
+        for (var j = 0; j < unit[1].length && val > 0; j++) {
+            p = digit[val % 10] + unit[1][j] + p;
             val = Math.floor(val / 10);
         }
-        s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + units[0][i] + s;
+        s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
     }
-    return s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^$/, '零元') + '整';
+    return head + s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整');
+};
+
+/**
+ * 将数值四舍五入(保留2位小数)后格式化成金额形式
+ * @param val
+ * @returns {string}
+ */
+$Utils.formatCurrency = function (val) {
+    val = val.toString().replace(/\$|\,/g, '');
+    if (isNaN(val)) {
+        val = '0';
+    }
+    var sign = (val == (val = Math.abs(val)));
+    val = Math.floor(val * 100 + 0.50000000001);
+    var cents = val % 100;
+    val = Math.floor(val / 100).toString();
+    if (cents < 10) {
+        cents = '0' + cents;
+    }
+    for (var i = 0; i < Math.floor((val.length - (1 + i)) / 3); i++) {
+        val = val.substring(0, val.length - (4 * i + 3)) + ',' + val.substring(val.length - (4 * i + 3));
+    }
+    return (((sign) ? '' : '-') + val + '.' + cents);
+};
+
+/**
+ * 将数值四舍五入(保留1位小数)后格式化成金额形式
+ * @param val
+ * @returns {string}
+ */
+$Utils.formatCurrencyTenThou = function (val) {
+    val = val.toString().replace(/\$|\,/g, '');
+    if (isNaN(val)) {
+        val = '0';
+    }
+    var sign = (val == (val = Math.abs(val)));
+    val = Math.floor(val * 10 + 0.50000000001);
+    var cents = val % 10;
+    val = Math.floor(val / 10).toString();
+    for (var i = 0; i < Math.floor((val.length - (1 + i)) / 3); i++) {
+        val = val.substring(0, val.length - (4 * i + 3)) + ',' + val.substring(val.length - (4 * i + 3));
+    }
+    return (((sign) ? '' : '-') + val + '.' + cents);
 };
 
 /**
@@ -747,8 +799,14 @@ $Utils.getChineseMoney = function (val) {
  * @returns {Object}
  */
 $Utils.getGlobal = function () {
-  if (typeof self !== 'undefined') { return self; }
-  if (typeof window !== 'undefined') { return window; }
-  if (typeof global !== 'undefined') { return global; }
-  throw new Error('unable to locate global object');
+    if (typeof self !== 'undefined') {
+        return self;
+    }
+    if (typeof window !== 'undefined') {
+        return window;
+    }
+    if (typeof global !== 'undefined') {
+        return global;
+    }
+    throw new Error('unable to locate global object');
 };
